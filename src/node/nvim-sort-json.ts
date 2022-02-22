c5c3d6b6590828fe5ebebe0986b1dc68e53b5de9
diartyz <diartyz@gmail.com>
2022-02-22 18:00:05 +0800

    refactor: enable ts

---
+++ src/node/nvim-sort-json.ts
@@ -0,0 +1,39 @@
+import { NvimPlugin } from 'neovim'
+import { sortJSON } from '../../lib/sort-json-utils'
+
+module.exports = (plugin: NvimPlugin) => {
+  plugin.setOptions({
+    dev: false,
+  })
+  plugin.registerCommand(
+    'SortJSON',
+    async () => {
+      try {
+        const options = await plugin.nvim.getVar('sort_json')
+        const content = await plugin.nvim.buffer.getLines({
+          start: 0,
+          end: -1,
+          strictIndexing: true,
+        })
+
+        await plugin.nvim.buffer.setLines(
+          JSON.stringify(
+            sortJSON(JSON.parse(content.join('')), ['asc'], options),
+            undefined,
+            2,
+          ).split('\n'),
+          {
+            start: 0,
+            end: -1,
+            strictIndexing: true,
+          },
+        )
+      } catch (err) {
+        await plugin.nvim.outWrite(`${err}\n`)
+      }
+    },
+    {
+      sync: false,
+    },
+  )
+}
